6a120db321ca41cec8beda634dd23014a3724a5e
edwardgurney <edwardgurney1979@gmail.com>
2021-10-23 18:02:28 +0000

    add select random moves function

--- scripts/game.js
+++ scripts/game.js
@@ -5,4 +5,22 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 }
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn };
